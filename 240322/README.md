bdbdb94b43e82c434d321258024ed95adc2b9452
권하진 <hajin@hajin-Mac-mini.local>
2024-03-22 22:13:19 +0900

    refactor: refactoring user code

--- 240322/README.md
+++ 240322/README.md
@@ -17,7 +17,7 @@ find() 메소드는 배열에서 주어진 함수를 만족하는 첫 번째 요
 <br><br>
 
 ## == vs ===
-== 연산자는 Equal Operator, === 연산자는 Strict Equal Operation
+== 연산자는 Equal Operator, === 연산자는 Strict Equal Operator
 
 ### == 연산자
 피연산자의 값을 비교해서 같으면 true, 다르면 false 반환
@@ -44,3 +44,26 @@ null == undefined  // true
 true === "true"     // false
 null === undefined  // false
 ```
+
+<br><br>
+
+## HTTP status code
+HTTP에서 클라이언트가 서버에게 request를 보내면 서버가 response를 보냄
+
+1XX : Informational Response
+- 100(Continue) : 요청의 시작 부분이 받아들여졌고, 클라이언트는 계속 이어서 보내야 함
+
+2XX : Success
+- 200(OK) : (조회, 수정, 삭제)요청을 정상적으로 처리
+- 201(Created) : 생성에 대한 요청을 받았으며 서버가 새 리소스를 작성함
+- 202(Accepted) : 요청을 접수했지만 아직 처리하지 않음
+
+3XX : Redirection
+- 300(Multiple Choice) : 클라이언트가 동시에 여러 응답이 가능한 요청을 보냈을 경우 클라이언트의 선택지를 반환
+
+4XX : Client Errors
+- 400(Bad Request) : 잘못된 문법으로 요청을 보내고 있어서 서버가 이해할 수 없음
+- 404(Not Found) : 요청한 URL을 찾을 수 없음
+
+5XX : server errors
+- 500(Internal Server Error) : 서버의 문제로 응답할 수 없음
---
+++ 240322/user.js
@@ -0,0 +1,134 @@
+// express 모듈 세팅
+const express = require('express')
+const app = express()
+
+app.listen(3000)
+app.use(express.json())
+
+// 사용자 정보를 저장할 데이터베이스
+// key = id, value = {name, age, password}
+let db = new Map();
+
+// REST API 설계 - 전체 사용자 조회
+app.get('/users', (req, res) => {
+  let users = {}
+
+  // 등록된 사용자가 1명 이상이면 전체 사용자 정보 출력
+  if (db.size) {
+    db.forEach((value, key) => {
+      {users[key] = value}
+    })
+    res.json(users);
+  }
+  // 등록된 사용자가 없으면, 에러 메시지
+  else {
+    res.status(404).json({
+      message : "no users to view"
+    })
+  }
+})
+
+// REST API 설계 - 개별 사용자 조회
+app.get('/users/:id', (req, res) => {
+  const {id} = req.params
+  const user = db.get(id)
+
+  // 사용자가 존재할 경우 사용자 정보 출력
+  if (user) {
+    res.json(user)
+  } 
+  // 사용자가 존재하지 않을 경우 에러 메세지
+  else {
+    res.status(404).json({
+      message : `${id} is not registered`
+    })
+  }
+})
+
+// REST API 설계 - 사용자 등록
+app.post('/users', (req, res) => {
+  const {id, name, age, password} = req.body
+  const exists = db.get(id)
+
+  // 사용자가 존재하지 않을 경우 사용자 등록
+  if (!exists) {
+    db.set(id, {"name" : name, "age" : age, "password" : password})
+    res.status(201).send({
+      message : `${id} registered successfully`
+    })
+  }
+  // body에 id 값이 넘어오지 않았을 경우
+  else if (!id) {
+    res.status(400).json({
+      message : "request value is invalid"
+    })
+  }
+  // 사용자가 존재할 경우 에러 메세지
+  else {
+    res.status(404).json({
+      message : `${id} already exists`
+    })
+  }
+})
+
+// REST API 설계 - 개별 사용자 삭제
+app.delete('/users/:id', (req, res) => {
+  const {id} = req.params
+  const user = db.get(id)
+  
+  // 사용자가 존재할 경우 사용자 제거
+  if (user) {
+    db.delete(id)
+    res.json({
+      message : `${id} is deleted`
+    })
+  } 
+  // 사용자가 존재하지 않을 경우 에러 메시지
+  else {
+    res.status(404).json({
+      message : `${id} is not registered`
+    })
+  }
+})
+
+// REST API 설계 - 전체 사용자 삭제
+app.delete('/users', (req, res) => {
+  // 등록된 사용자가 1명 이상이면 전체 삭제
+  if (user.size) {
+    db.clear()
+    res.json({
+      message : "all users deleted"
+    })
+  }
+  // 등록된 사용자가 없으면, 에러 메시지
+  else {
+    res.status(404).json({
+      message : "no users to delete"
+    })
+  }
+})
+
+// REST API 설계 - 개별 사용자 수정
+app.put('/users/:id', (req, res) => {
+  const {id} = req.params
+  const user = db.get(id)
+  
+  // 사용자가 존재할 경우 비밀번호 변경
+  if (user) {
+    const oldPassword = user.password
+    const newPassword = req.body.password
+
+    user.password = newPassword
+    db.set(id, user)
+
+    res.json({
+      message : "password changed successfully"
+    })
+  } 
+  // 사용자가 존재하지 않을 경우 에러 메시지
+  else {
+    res.status(404).json({
+      message : `${id} is not registered`
+    })
+  }
+})
