e038889d531a57a1c37bfd4ee1f43bbf1bbd0207
hajin0324 <hajinn24@gmail.com>
2024-05-17 14:38:27 +0900

    Feat: task modal 구현

--- 06-react-task-app/src/store/slices/boardsSlice.ts
+++ 06-react-task-app/src/store/slices/boardsSlice.ts
@@ -26,12 +26,18 @@ type TAddTaskAction = {
   task: ITask;
 }
 
+type TDeleteTaskAction = {
+  boardId: string;
+  listId: string;
+  taskId: string;
+}
+
 const initialState: TBoardState = {
   modalActive: false,
   boardArray: [
     {
       boardId: 'board-0',
-      boardName: "기본 게시판",
+      boardName: "default board",
       lists: [
         {
           listId: "list-0",
@@ -102,6 +108,52 @@ const boardsSlice = createSlice({
       )
     },
 
+    updateTask: (state, { payload }: PayloadAction<TAddTaskAction>) => {
+      state.boardArray = state.boardArray.map(
+        board => board.boardId === payload.boardId ?
+        {
+          ...board,
+          lists: board.lists.map(list =>
+            list.listId === payload.listId ?
+            {
+              ...list,
+              tasks: list.tasks.map(task => 
+                task.taskId === payload.task.taskId
+                ? payload.task
+                : task
+              )
+            }
+            :
+            list
+          )
+        }
+        :
+        board
+      )
+    },
+
+    deleteTask: (state, { payload }: PayloadAction<TDeleteTaskAction>) => {
+      state.boardArray = state.boardArray.map(board =>
+        board.boardId === payload.boardId ?
+        {
+          ...board,
+          lists: board.lists.map(list => 
+            list.listId === payload.listId ?
+            {
+              ...list,
+              tasks: list.tasks.filter(
+                task => task.taskId !== payload.taskId
+              )
+            }
+            :
+            list
+          )
+        }
+        :
+        board
+      )
+    },
+
     deleteList: (state, { payload }: PayloadAction<TDeleteListAction>) => {
       state.boardArray = state.boardArray.map(
         board => board.boardId === payload.boardId ?
@@ -122,5 +174,5 @@ const boardsSlice = createSlice({
   }
 })
 
-export const { addBoard, deleteList, setModalActive, addList, addTask }= boardsSlice.actions;
+export const { addBoard, deleteList, setModalActive, addList, addTask, updateTask, deleteTask } = boardsSlice.actions;
 export const boardsReducer = boardsSlice.reducer;
