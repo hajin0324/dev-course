07eeca249c034f399496946c9cd36c4be656b983
hajin0324 <hajinn24@gmail.com>
2024-05-17 14:49:41 +0900

    Feat: 활용 목록 modal 구현

--- 06-react-task-app/src/App.tsx
+++ 06-react-task-app/src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
-import { appContainer, board, buttons } from './App.css'
+import { appContainer, board, buttons, deleteBoardBtn, loggerBtn } from './App.css'
 import BoardList from './components/BoardList/BoardList'
 import ListsContainer from './components/ListsConatiner/ListsContainer';
 import { useTypedSelector } from './hooks/redux';
+import EditModal from './components/EditModal/EditModal';
+import LoggerModal from './components/LoggerModal/LoggerModal';
 
 function App() {
+  const [isLoggerOpen, setIsLoggerOpen] = useState(false);
   const [activeBoardId, setActiveBoardId] = useState('board-0');
+  const modalActive = useTypedSelector(state => state.boards.modalActive);
 
   const boards = useTypedSelector(state => state.boards.boardArray);
 
@@ -15,6 +19,9 @@ function App() {
 
   return (
     <div className={appContainer}>
+      {isLoggerOpen ? <LoggerModal setIsLoggerOpen={setIsLoggerOpen} /> : null}
+      {modalActive ? <EditModal /> : null}
+
       <BoardList 
           activeBoardId={activeBoardId} 
           setActiveBoardId={setActiveBoardId} 
@@ -24,11 +31,14 @@ function App() {
       </div>
 
       <div className={buttons}>
-        <button>
+        <button className={deleteBoardBtn}>
           Delete this board
         </button>
-        <button>
-          Show activity list
+        <button 
+          className={loggerBtn}
+          onClick={() => setIsLoggerOpen(!isLoggerOpen)}
+        >
+          {isLoggerOpen ? 'Hide activity list' : 'Show activity list'}
         </button>
       </div>
     </div>
